80d333edf3a666f8501f5d4d61fc170deb52f7be
ArduinoTechsDev <9836910+ToonAutomation@users.noreply.github.com>
2018-05-29 17:24:28 +0700

    Update TaskProcess.cpp

--- TaskProcess/TaskProcess.cpp
+++ TaskProcess/TaskProcess.cpp
@@ -1,1 +1,49 @@
 
+#include "TaskProcess.h"
+
+#define Arduino
+
+#ifdef Arduino
+#define TimeCurrent millis()
+#endif // 
+
+#ifdef Windows
+#include "Windows.h"
+#define TimeCurrent millis()
+
+WORD millis()
+{
+	SYSTEMTIME t;
+	GetSystemTime(&t);
+	return (t.wSecond * 1000) + t.wMilliseconds;
+}
+
+#endif // Windows
+
+
+
+
+void TaskProcess::init(unsigned long interval_ms,callback function)
+{
+	
+	jobprocess = function;
+	Tinterval = interval_ms;
+	TStart = TimeCurrent + Tinterval;
+	RunState = true;
+}
+
+bool TaskProcess::Run()
+{
+	unsigned long TCurrent = TimeCurrent;
+	if ((TCurrent >= TStart)&&(RunState))
+	{
+		jobprocess();
+		TStart = TCurrent + Tinterval;
+		return true;
+	}
+	return false;
+}
+
+
+
+
